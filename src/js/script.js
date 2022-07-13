220847208d3a2e92d979bb53c79fd3b78f5d496e
Ivan Metlitski <moonydis@gmail.com>
2022-07-13 15:11:55 +0300

    smooth scroll

--- src/js/script.js
+++ src/js/script.js
@@ -16,5 +16,18 @@ percents.forEach((item, i)=>{
     lines[i].style.width = item.innerHTML
 })
 
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
+
 
 
